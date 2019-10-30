34c21dbe31fefb451d91a205c2bc550fec7b0792
mpizarro <maximilianopi@buenosaires.gob.ar>
2019-10-30 10:24:30 -0300

    Initial application generated by JHipster-5.8.2

---
+++ src/main/java/com/carpetaciudadana/app/config/FeignConfiguration.java
@@ -0,0 +1,18 @@
+package com.carpetaciudadana.app.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.carpetaciudadana.app")
+public class FeignConfiguration {
+
+    /**
+     * Set the Feign specific log level to log client REST requests
+     */
+    @Bean
+    feign.Logger.Level feignLoggerLevel() {
+        return feign.Logger.Level.BASIC;
+    }
+}
