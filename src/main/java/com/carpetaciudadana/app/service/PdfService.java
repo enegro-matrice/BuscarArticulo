a45a3d1c9074c5d47c45cb482fd8ed6a85c295e4
enegro-matrice <enegro@matriceconsulting.com.ar>
2019-11-14 09:06:38 -0300

    14/11

--- src/main/java/com/carpetaciudadana/app/service/PdfService.java
+++ src/main/java/com/carpetaciudadana/app/service/PdfService.java
@@ -4,9 +4,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Set;
 
+import freemarker.core.ParseException;
 import freemarker.template.TemplateException;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.itextpdf.text.DocumentException;
 
+import org.springframework.boot.json.JsonParseException;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface PdfService {
@@ -16,15 +21,15 @@ public interface PdfService {
 
 	public File generatePDFFromHTML(String filename, String fileExt, File html, Boolean alineacionHorizontal) throws DocumentException, IOException;
 
-	public String savePNG(MultipartFile files, String template) throws IOException;
+	public String savePNG(MultipartFile files, String template) throws IOException, JsonMappingException, JsonParseException, NotFoundException, ParseException;
 
 //	public String setCertificado(MultipartFile files, String template) throws IOException;
 	
-	public Set<String> getListFiles(String template) throws IOException;
+	public Set<String> getListFiles(String template) throws IOException, JsonMappingException, JsonParseException, NotFoundException, ParseException;
 	
-	public Set<String> getCertificado(String template) throws IOException;
+	public Set<String> getCertificado(String template) throws IOException, JsonMappingException, JsonParseException, NotFoundException, ParseException;
 
-	public String deleteFiles(String name,String template) throws IOException;
+	public String deleteFiles(String name,String template) throws IOException, JsonMappingException, JsonParseException, NotFoundException, ParseException;
 	
-	public File generatePDFFromBase64(String filename, String fileExt,String file) throws IOException;
+	public File generatePDFFromBase64(String filename, String fileExt,String file) throws IOException, JsonMappingException, JsonParseException, NotFoundException, ParseException;
 }
--- src/main/java/com/carpetaciudadana/app/web/rest/TemplateResource.java
+++ src/main/java/com/carpetaciudadana/app/web/rest/TemplateResource.java
@@ -7,11 +7,15 @@ import java.util.Set;
 import com.carpetaciudadana.app.domain.EstudianteCedel;
 import com.carpetaciudadana.app.service.PdfService;
 import com.carpetaciudadana.app.service.dto.TituloCedelDTOpdf;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.hazelcast.com.eclipsesource.json.ParseException;
 import com.itextpdf.text.DocumentException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -57,7 +61,7 @@ public class TemplateResource {
         ,true).toPath());
     }
     @PostMapping(value = "/file/setmedia", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public String setpngmedia(@RequestParam(value = "files", required = true) MultipartFile files) throws IOException{
+    public String setpngmedia(@RequestParam(value = "files", required = true) MultipartFile files) throws IOException, JsonMappingException, JsonParseException, NotFoundException, ParseException{
         return pdfService.savePNG(files, template) + " fue guardado con exito";
     }
    /* @PostMapping(value = "/file/Resource", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -65,16 +69,16 @@ public class TemplateResource {
         return pdfService.setCertificado(files,template) + " fue guardado con exito";
     }*/
     @GetMapping(value = "/file/Resource")
-    public ResponseEntity<Set<String>> getCertificado() throws IOException{  
+    public ResponseEntity<Set<String>> getCertificado() throws IOException, JsonMappingException, JsonParseException, NotFoundException, ParseException{  
         return ResponseEntity.accepted().body(pdfService.getCertificado(template));
         //return new ResponseEntity<>(pdfService.getCertificado(template), HttpStatus.OK);
     }
     @GetMapping(value = "/file/getListResource")
-    public ResponseEntity<Set<String>> getListFiles() throws IOException{
+    public ResponseEntity<Set<String>> getListFiles() throws IOException, JsonMappingException, JsonParseException, NotFoundException, ParseException{
         return ResponseEntity.accepted().body(pdfService.getListFiles(template));
     }
     @PostMapping(value = "/file/DeletMedia")
-    public ResponseEntity<String> DelMedia(@RequestParam(value = "name",required = true) String name) throws IOException{
+    public ResponseEntity<String> DelMedia(@RequestParam(value = "name",required = true) String name) throws IOException, JsonMappingException, JsonParseException, NotFoundException, ParseException{
         return ResponseEntity.accepted().body(pdfService.deleteFiles(name, template));
     }
   
